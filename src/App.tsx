d0d12cfa6143e6ba55d06c0a9801f03afaa9feef
Koyyada Pranayan <koyyadapranayan@gmail.com>
2024-12-21 12:45:29 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Products from './components/Products'
+import EmpSal from './components/EmpSat'
+import EmpApi from './components/EmpApi'
+export default function App() {
+  return (
+    <div>
+      <Products/>
+      <EmpSal/>
+      <EmpApi/>
+    </div>
+  )
+}
